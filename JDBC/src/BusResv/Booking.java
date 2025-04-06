fad0221447e8057d110ce7cb0928732a42992bf5
ArrchitRamanaMS-2004 <arrchith26@gmail.com>
2025-04-06 10:42:03 +0530

    bus reservation sytem using oops concepts completed

--- JDBC/src/BusResv/Booking.java
+++ JDBC/src/BusResv/Booking.java
@@ -1,9 +1,49 @@
 package BusResv;
 
-import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Booking {
     private String passengerName;
-    private int busNo;
+    private final int busNo;
     private Date date;
+    public Booking (){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Passenger Name : ");
+        this.passengerName = sc.nextLine();
+        System.out.println("Enter Bus Number : ");
+        this.busNo = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter departure date (dd-MM-yyyy) : ");
+        String dateInput = sc.nextLine();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            this.date = dateFormat.parse(dateInput);
+        } catch (ParseException e) {
+            throw new RuntimeException("Invalid date format",e);
+        }
+    }
+    public String getPassengerName(){ return passengerName; }
+    public int getBusNo() { return busNo; }
+    public Date getDate() { return date; }
+    public void setPassengerName(String passengerName){ this.passengerName = passengerName; }
+    public void setDate(Date date) { this.date = date; }
+
+    public boolean isAvailable(ArrayList<Bus> buses,ArrayList<Booking> bookings){
+        int checkCapacity = 0;
+        for ( Bus bus : buses ) {
+            if ( this.busNo == bus.getBusNo() ) {
+                checkCapacity = bus.getCapacity();
+            }
+        }
+        //checking no of bookings already happened in the bus ( which we want to include our booking ) and in the date ( in which we want to include our booking )
+        int booked = 0;
+        for (Booking bookedTickets : bookings){
+            if (bookedTickets.getBusNo() == this.busNo && bookedTickets.getDate().equals(this.date)){
+                booked++;
+            }
+        }
+        return booked < checkCapacity ? true : false;
+    }
 }
--- JDBC/src/BusResv/BusDemo.java
+++ JDBC/src/BusResv/BusDemo.java
@@ -7,8 +7,9 @@ public class BusDemo {
     public static void main(String[] args) {
 
         ArrayList<Bus> buses = new ArrayList<Bus>();
+        ArrayList<Booking> bookings = new ArrayList<Booking>();
 
-        buses.add(new Bus(1,true,45));
+        buses.add(new Bus(1,true,2));
         buses.add(new Bus(2,false,50));
         buses.add(new Bus(3,true,48));
 
@@ -22,8 +23,14 @@ public class BusDemo {
         while (userOpt == 1){
             System.out.println("Enter 1 to book and 2 to exit");
             userOpt = sc.nextInt();
-            if (userOpt==1)
-                System.out.println("booking");
+            if (userOpt==1) {
+                Booking booking = new Booking();
+                if (booking.isAvailable(buses, bookings)) {
+                    bookings.add(booking);
+                } else {
+                    System.out.println("Sorry , the tickets are reserved. Please try another Bus or Date");
+                }
+            }
         }
     }
 }
